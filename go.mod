1c28013c8f5289dbbc4daa38e61a6a34526decc0
Xuejie Xiao <xxuejie@gmail.com>
2019-02-09 16:56:04 +0800

    WebSocket setup

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/xxuejie/paguridae
 
-require github.com/fmpwizard/go-quilljs-delta v0.0.2
+require (
+	github.com/fmpwizard/go-quilljs-delta v0.0.2
+	github.com/gorilla/websocket v1.4.0
+)
--- main.go
+++ main.go
@@ -5,13 +5,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 var port = flag.Int("port", 8000, "port to listen for http server")
 var useLocalAsset = flag.Bool("useLocalAsset", false, "development only, you shouldn't use true in production")
 
+var upgrader = websocket.Upgrader{}
+
+func webSocketHandler(w http.ResponseWriter, req *http.Request) {
+	_, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Print("Error upgrading websocket:", err)
+		return
+	}
+	log.Print("Websocket connection established!")
+}
+
 func main() {
 	flag.Parse()
+	http.HandleFunc("/ws", webSocketHandler)
 	http.Handle("/", http.FileServer(FS(*useLocalAsset)))
 	log.Printf("Starting server on port: %d", *port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
