ab0d79b0575057e7b41d42b212f25c9b06c4280d
Xuejie Xiao <xxuejie@gmail.com>
2020-05-05 08:00:22 +0000

    Reorganize file system code

--- cmd/paguridae/file_system.go
+++ cmd/paguridae/file_system.go
@@ -119,7 +119,7 @@ var fileinfos = map[uint32]fileinfo{
 }
 
 func Serve9PFileSystem(listener net.Listener, quitSignal chan bool, server *ot.MultiFileServer) error {
-	user, err := user.Current()
+	currentUser, err := user.Current()
 	if err != nil {
 		return err
 	}
@@ -136,156 +136,158 @@ Loop:
 			log.Printf("Accepting error: %v", err)
 			continue
 		}
-		go func(c net.Conn) {
-			// Tversion
-			fcall, err := plan9.ReadFcall(c)
-			if err != nil {
-				log.Printf("Error reading version message: %v", err)
-				c.Close()
-				return
-			}
-			if fcall.Type != plan9.Tversion ||
-				fcall.Version != "9P2000" {
-				log.Printf("Invalid version message: %s", fcall)
-				c.Close()
-				return
-			}
-			err = plan9.WriteFcall(c, &plan9.Fcall{
-				Type:    plan9.Rversion,
-				Tag:     fcall.Tag,
-				Msize:   fcall.Msize,
-				Version: fcall.Version,
-			})
-			if err != nil {
-				log.Printf("Error writing version reply: %v", err)
-				c.Close()
-				return
-			}
-			// Tauth
-			fcall, err = plan9.ReadFcall(c)
-			if err != nil {
-				log.Printf("Error reading auth message: %v", err)
-				c.Close()
-				return
-			}
-			if fcall.Type != plan9.Tauth {
-				log.Printf("Invalid auth message: %s", fcall)
-				c.Close()
-				return
-			}
-			openedFiles := make(map[uint32]plan9.Qid)
-			openedFiles[fcall.Afid] = plan9.Qid{
-				Path: PATH_TYPE_ROOT,
-				Vers: 0,
-				Type: plan9.QTDIR,
-			}
-			err = plan9.WriteFcall(c, &plan9.Fcall{
-				Type: plan9.Rauth,
-				Tag:  fcall.Tag,
-				Aqid: openedFiles[fcall.Afid],
-			})
-			if err != nil {
-				log.Printf("Error writing auth reply: %v", err)
-				c.Close()
-				return
-			}
-			for {
-				fcall, err := plan9.ReadFcall(c)
-				if err != nil {
-					log.Printf("Invalid message: %v", err)
-					c.Close()
-					return
-				}
-				response := plan9.Fcall{
-					Type:  plan9.Rerror,
-					Tag:   fcall.Tag,
-					Ename: "Unknown error",
-				}
-				switch fcall.Type {
-				default:
-					log.Printf("Unknown fcall: %s", fcall)
-					response.Ename = fmt.Sprintf("Unknown fcall: %d", fcall.Type)
-				case plan9.Tattach:
-					qid, ok := openedFiles[fcall.Afid]
-					if !ok {
-						response.Ename = fmt.Sprintf("Afid %d is not assigned!", fcall.Afid)
-						break
-					}
-					_, ok = openedFiles[fcall.Fid]
-					if ok {
-						response.Ename = fmt.Sprintf("Fid %d has already been used", fcall.Fid)
-						break
-					}
-					openedFiles[fcall.Fid] = qid
-					response.Type = plan9.Rattach
-					response.Qid = qid
-				case plan9.Tclunk:
-					delete(openedFiles, fcall.Fid)
-					response.Type = plan9.Rclunk
-				case plan9.Tstat:
-					qid, ok := openedFiles[fcall.Fid]
-					if !ok {
-						response.Ename = fmt.Sprintf("Fid %d is not assigned!", fcall.Fid)
-						break
-					}
-					fileinfo := fileinfos[uint32(qid.Path)]
-					t := time.Now().Unix()
-					dir := plan9.Dir{
-						Type:  uint16(fileinfo.Type),
-						Dev:   0,
-						Qid:   qid,
-						Mode:  plan9.Perm(fileinfo.Perm),
-						Atime: uint32(t),
-						Mtime: uint32(t),
-						// Right now we are copying plan9port's acme behavior
-						Length: 0,
-						Name:   fileinfo.Name,
-						Uid:    user.Uid,
-						Gid:    user.Gid,
-						Muid:   user.Uid,
-					}
-					response.Stat, _ = dir.Bytes()
-					response.Type = plan9.Rstat
-				case plan9.Twalk:
-					qid, ok := openedFiles[fcall.Fid]
-					if !ok {
-						response.Ename = fmt.Sprintf("Fid %d is not assigned!", fcall.Fid)
-						break
-					}
-					_, ok = openedFiles[fcall.Newfid]
-					if ok {
-						response.Ename = fmt.Sprintf("Newfid %d has already been used", fcall.Newfid)
-						break
-					}
-					saveNewfid := true
-					if len(fcall.Wname) > 0 {
-						qids := walk(qid, fcall.Wname, server)
-						if len(qids) == 0 {
-							response.Ename = fmt.Sprintf("Unable to walk to: %s", fcall.Wname[0])
-							break
-						}
-						response.Wqid = qids
-						if len(qids) != len(fcall.Wname) {
-							saveNewfid = false
-						} else {
-							qid = qids[len(qids)-1]
-						}
-					}
-					if saveNewfid {
-						openedFiles[fcall.Newfid] = qid
-					}
-					response.Type = plan9.Rwalk
-				}
-				err = plan9.WriteFcall(c, &response)
-				if err != nil {
-					log.Printf("Error writing auth reply: %v", err)
-					c.Close()
-					return
-				}
-			}
-		}(conn)
+		go loop(conn, server, currentUser)
 	}
 	return nil
+}
+
+func loop(c net.Conn, server *ot.MultiFileServer, currentUser *user.User) {
+	// Tversion
+	fcall, err := plan9.ReadFcall(c)
+	if err != nil {
+		log.Printf("Error reading version message: %v", err)
+		c.Close()
+		return
+	}
+	if fcall.Type != plan9.Tversion ||
+		fcall.Version != "9P2000" {
+		log.Printf("Invalid version message: %s", fcall)
+		c.Close()
+		return
+	}
+	err = plan9.WriteFcall(c, &plan9.Fcall{
+		Type:    plan9.Rversion,
+		Tag:     fcall.Tag,
+		Msize:   fcall.Msize,
+		Version: fcall.Version,
+	})
+	if err != nil {
+		log.Printf("Error writing version reply: %v", err)
+		c.Close()
+		return
+	}
+	// Tauth
+	fcall, err = plan9.ReadFcall(c)
+	if err != nil {
+		log.Printf("Error reading auth message: %v", err)
+		c.Close()
+		return
+	}
+	if fcall.Type != plan9.Tauth {
+		log.Printf("Invalid auth message: %s", fcall)
+		c.Close()
+		return
+	}
+	allocedFiles := make(map[uint32]plan9.Qid)
+	allocedFiles[fcall.Afid] = plan9.Qid{
+		Path: PATH_TYPE_ROOT,
+		Vers: 0,
+		Type: plan9.QTDIR,
+	}
+	err = plan9.WriteFcall(c, &plan9.Fcall{
+		Type: plan9.Rauth,
+		Tag:  fcall.Tag,
+		Aqid: allocedFiles[fcall.Afid],
+	})
+	if err != nil {
+		log.Printf("Error writing auth reply: %v", err)
+		c.Close()
+		return
+	}
+	for {
+		fcall, err := plan9.ReadFcall(c)
+		if err != nil {
+			log.Printf("Invalid message: %v", err)
+			c.Close()
+			return
+		}
+		response := plan9.Fcall{
+			Type:  plan9.Rerror,
+			Tag:   fcall.Tag,
+			Ename: "Unknown error",
+		}
+		switch fcall.Type {
+		default:
+			log.Printf("Unknown fcall: %s", fcall)
+			response.Ename = fmt.Sprintf("Unknown fcall: %d", fcall.Type)
+		case plan9.Tattach:
+			qid, ok := allocedFiles[fcall.Afid]
+			if !ok {
+				response.Ename = fmt.Sprintf("Afid %d is not assigned!", fcall.Afid)
+				break
+			}
+			_, ok = allocedFiles[fcall.Fid]
+			if ok {
+				response.Ename = fmt.Sprintf("Fid %d has already been used", fcall.Fid)
+				break
+			}
+			allocedFiles[fcall.Fid] = qid
+			response.Type = plan9.Rattach
+			response.Qid = qid
+		case plan9.Tclunk:
+			delete(allocedFiles, fcall.Fid)
+			response.Type = plan9.Rclunk
+		case plan9.Tstat:
+			qid, ok := allocedFiles[fcall.Fid]
+			if !ok {
+				response.Ename = fmt.Sprintf("Fid %d is not assigned!", fcall.Fid)
+				break
+			}
+			fileinfo := fileinfos[uint32(qid.Path)]
+			t := time.Now().Unix()
+			dir := plan9.Dir{
+				Type:  uint16(fileinfo.Type),
+				Dev:   0,
+				Qid:   qid,
+				Mode:  plan9.Perm(fileinfo.Perm),
+				Atime: uint32(t),
+				Mtime: uint32(t),
+				// Right now we are copying plan9port's acme behavior
+				Length: 0,
+				Name:   fileinfo.Name,
+				Uid:    currentUser.Uid,
+				Gid:    currentUser.Gid,
+				Muid:   currentUser.Uid,
+			}
+			response.Stat, _ = dir.Bytes()
+			response.Type = plan9.Rstat
+		case plan9.Twalk:
+			qid, ok := allocedFiles[fcall.Fid]
+			if !ok {
+				response.Ename = fmt.Sprintf("Fid %d is not assigned!", fcall.Fid)
+				break
+			}
+			_, ok = allocedFiles[fcall.Newfid]
+			if ok {
+				response.Ename = fmt.Sprintf("Newfid %d has already been used", fcall.Newfid)
+				break
+			}
+			saveNewfid := true
+			if len(fcall.Wname) > 0 {
+				qids := walk(qid, fcall.Wname, server)
+				if len(qids) == 0 {
+					response.Ename = fmt.Sprintf("Unable to walk to: %s", fcall.Wname[0])
+					break
+				}
+				response.Wqid = qids
+				if len(qids) != len(fcall.Wname) {
+					saveNewfid = false
+				} else {
+					qid = qids[len(qids)-1]
+				}
+			}
+			if saveNewfid {
+				allocedFiles[fcall.Newfid] = qid
+			}
+			response.Type = plan9.Rwalk
+		}
+		err = plan9.WriteFcall(c, &response)
+		if err != nil {
+			log.Printf("Error writing auth reply: %v", err)
+			c.Close()
+			return
+		}
+	}
 }
 
 func walk(start plan9.Qid, wnames []string, server *ot.MultiFileServer) []plan9.Qid {
