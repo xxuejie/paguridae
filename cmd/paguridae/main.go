8f4db68aa2f5eb471741a2d9ce34160253f18934
Xuejie Xiao <xxuejie@gmail.com>
2020-05-21 12:45:21 +0000

    Add HTTPS handling via Let's Encrypt

--- cmd/paguridae/main.go
+++ cmd/paguridae/main.go
@@ -1,6 +1,8 @@
 package main // import "github.com/xxuejie/paguridae"
 
 import (
+	"context"
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -10,8 +12,10 @@ import (
 	"os/signal"
 	"regexp"
 	"syscall"
+	"time"
 
 	"github.com/NYTimes/gziphandler"
+	"golang.org/x/crypto/acme/autocert"
 	"nhooyr.io/websocket"
 )
 
@@ -19,6 +23,10 @@ var port = flag.Int("port", 8000, "port to listen for http server")
 var customStyleFile = flag.String("customStyleFile", "", "file containing custom styles to apply, notice this won't work when useLocalAsset is true")
 var useLocalAsset = flag.Bool("useLocalAsset", false, "development only, you shouldn't use true in production")
 var verifyContent = flag.Bool("verifyContent", false, "development only, set to true to enable content verification")
+var useHttps = flag.Bool("useHttps", false, "listen on 443 port for HTTPS requests")
+var domain = flag.String("domain", "", "Domain to use for generating letsencrypt certificates")
+var certCache = flag.String("certCache", "./certs", "Cached directory for certificates")
+var redirectToHttps = flag.Bool("redirectoToHttps", true, "Redirect HTTP request to HTTPS request, only enabled when useHttps is also enabled")
 
 func webSocketHandler(w http.ResponseWriter, req *http.Request) {
 	c, err := websocket.Accept(w, req, websocket.AcceptOptions{})
@@ -41,6 +49,33 @@ func webSocketHandler(w http.ResponseWriter, req *http.Request) {
 	connection.Stop()
 }
 
+// HTTPS handling logic is adapted from https://github.com/kjk/go-cookbook/blob/13bbc271f500ec28f21ebc28b82ac985b7e4bffd/free-ssl-certificates/main.go
+func makeServerFromMux(mux *http.ServeMux) *http.Server {
+	return &http.Server{
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Handler:      mux,
+	}
+}
+
+func makeHTTPServer() *http.Server {
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/ws", webSocketHandler)
+	mux.Handle("/", gziphandler.GzipHandler(http.FileServer(FS(*useLocalAsset))))
+	return makeServerFromMux(mux)
+}
+
+func makeRedirectServer() *http.Server {
+	handleRedirect := func(w http.ResponseWriter, r *http.Request) {
+		newURI := "https://" + r.Host + r.URL.String()
+		http.Redirect(w, r, newURI, http.StatusFound)
+	}
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handleRedirect)
+	return makeServerFromMux(mux)
+}
+
 func main() {
 	flag.Parse()
 	patches := make(map[string]string)
@@ -57,8 +92,6 @@ func main() {
 			log.Fatal(err)
 		}
 	}
-	http.HandleFunc("/ws", webSocketHandler)
-	http.Handle("/", gziphandler.GzipHandler(http.FileServer(FS(*useLocalAsset))))
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
@@ -66,6 +99,47 @@ func main() {
 		os.RemoveAll("/tmp/paguridae")
 		os.Exit(0)
 	}()
-	log.Printf("Starting server on port: %d", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+
+	// Initialize HTTP(s) servers
+	var m *autocert.Manager
+	if *useHttps {
+		hostPolicy := func(ctx context.Context, host string) error {
+			allowedHost := *domain
+			if host == allowedHost {
+				return nil
+			}
+			return fmt.Errorf("acme/autocert: only %s host is allowed", allowedHost)
+		}
+		m = &autocert.Manager{
+			Prompt:     autocert.AcceptTOS,
+			HostPolicy: hostPolicy,
+			Cache:      autocert.DirCache(*certCache),
+		}
+
+		httpsSrv := makeHTTPServer()
+		httpsSrv.Addr = ":443"
+		httpsSrv.TLSConfig = &tls.Config{GetCertificate: m.GetCertificate}
+
+		go func() {
+			log.Printf("Starting HTTPS server on %s", httpsSrv.Addr)
+			log.Fatal(httpsSrv.ListenAndServeTLS("", ""))
+		}()
+	}
+	if *useHttps && *port != 80 {
+		log.Printf("Warning: HTTPS is started, but HTTP server is not started on port 80, please make sure this is what you want")
+	}
+	var httpSrv *http.Server
+	if *useHttps && *redirectToHttps {
+		httpSrv = makeRedirectServer()
+	} else {
+		httpSrv = makeHTTPServer()
+	}
+	// allow autocert handle Let's Encrypt callbacks over http
+	if m != nil {
+		httpSrv.Handler = m.HTTPHandler(httpSrv.Handler)
+	}
+
+	httpSrv.Addr = fmt.Sprintf(":%d", *port)
+	log.Printf("Starting HTTP server on port: %d", *port)
+	log.Fatal(httpSrv.ListenAndServe())
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/tdewolff/parse v2.3.4+incompatible // indirect
 	github.com/tdewolff/test v1.0.6 // indirect
 	github.com/xxuejie/go-delta-ot v0.0.0-20200405082330-c109d451c13f
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898 // indirect
 	nhooyr.io/websocket v1.1.1
 	xuejie.space/c/go-quill-editor v0.0.0-20200413112626-1ee9caadfeae
